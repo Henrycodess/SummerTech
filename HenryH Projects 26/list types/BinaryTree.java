132f9f1455d8e6d6ce4ccb65f25c4c91712d7d0a
Your Name <you@example.com>
2026-07-30 12:59:40 -0400

    blehhhhh

--- HenryH Projects 26/list types/BinaryTree.java
+++ HenryH Projects 26/list types/BinaryTree.java
@@ -6,25 +6,104 @@ public class BinaryTree {
     public BinaryTree(int value){
         this.head  = new BinaryNode(value);
     }
+    public void printTree(){
+        printTree(head);
+    }
+    public void printTree(BinaryNode currentNode){
+        if (currentNode.getLeftNode() != null){
+            printTree(currentNode.getLeftNode());
+        }
+        System.out.println(currentNode.getValue());
+        if (currentNode.getRightNode() != null){
+            printTree(currentNode.getRightNode());
+        }
+    }
+    public void addNode(int value){
+        addNode(value, this.head);
+    }
     public void addNode(int value, BinaryNode currentNode){
         if (value < currentNode.getValue()){
-            currentNode = currentNode.getLeftNode();
-            if (currentNode == null){
-                currentNode = new BinaryNode(value);
+            if (currentNode.getLeftNode() == null){
+                currentNode.setLeftNode(new BinaryNode(value));
             }
             else{
-                addNode(value, currentNode);
+                addNode(value, currentNode.getLeftNode());
             }
         }
         else{
-            currentNode = currentNode.getRightNode();
-            if (currentNode == null){
-                currentNode = new BinaryNode(value);
+            if (currentNode.getRightNode() == null){
+                currentNode.setRightNode(new BinaryNode(value));
             }
             else{
-                addNode(value, currentNode);
+                addNode(value, currentNode.getRightNode());
             }
         }
     }
-}
-    
+    public BinaryNode searchNode(int value){
+        return searchNode(value, head);
+    }
+    public BinaryNode searchNode(int value, BinaryNode currentNode){
+        System.out.println(currentNode.getValue());
+        if (value < currentNode.getValue()){
+            return searchNode(value, currentNode.getLeftNode());
+        }
+        if (value > currentNode.getValue()){
+            return searchNode(value, currentNode.getRightNode());
+        }
+        else{
+            return currentNode;
+        }
+
+    }
+    public void searchDelete(int value){
+        searchDelete(value, head);
+    }
+    public void searchDelete(int value, BinaryNode currentNode){
+        if (value < currentNode.getValue()){
+            if (currentNode.getLeftNode() == null){
+                return;
+            }
+            else{
+                searchDelete(value, currentNode.getLeftNode());
+                if (currentNode.getLeftNode().getValue() == value){
+                    currentNode.setLeftNode(null);
+                }
+            }
+        }
+        else{
+            if (currentNode.getRightNode() == null){
+                return;
+            }
+            else{
+                searchDelete(value, currentNode.getRightNode());
+                if (currentNode.getRightNode().getValue() == value){
+                    currentNode.setRightNode(null);
+                }
+            }
+        }
+    }
+    public void deleteMiddle(int value, BinaryNode currentNode){
+        if (value < currentNode.getValue()){
+            if (currentNode.getLeftNode() == null){
+                return;
+            }
+            else{
+                searchDelete(value, currentNode.getLeftNode());
+                if (currentNode.getLeftNode().getValue() == value){
+                    currentNode.setLeftNode(null);
+                }
+            }
+        }
+        else{
+            if (currentNode.getRightNode() == null){
+                return;
+            }
+            else{
+                searchDelete(value, currentNode.getRightNode());
+                if (currentNode.getRightNode().getValue() == value){
+                    currentNode.setRightNode(null);
+                }
+            }
+        }
+    }
+}   
--- HenryH Projects 26/list types/Main.java
+++ HenryH Projects 26/list types/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        Stack stack = new Stack();
-        stack.push(0);
-        stack.push(2);
-        stack.push(1);
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
+        BinaryTree tree = new BinaryTree(4);
+        tree.addNode(5);
+        tree.addNode(9);
+        tree.addNode(6);
+        tree.addNode(2);
+        tree.addNode(3);
+        tree.addNode(1);
+        tree.printTree();
+        tree.searchDelete(1);
+        System.out.println("new tree");
+        tree.printTree();
     }
 }
